5bae65ac7b5a47048d7fe054ab4c3002b04224f9
SHIVAM GUPTA <shivamg11000@gmail.com>
2018-03-18 11:10:43 +0530

    changes some css

--- client/js/main.js
+++ client/js/main.js
@@ -13,32 +13,60 @@ function msgTemplate(avatarSrc, nickname, msg, timestamp){
     `;
 }
 
+// not used until now
+var profilePage = `
+    <div id="login-page">
+    <div class="login">
+        <div class="logo"><img src="https://image.flaticon.com/icons/svg/179/179608.svg"></div>
+        <form id="goggle+" action="">
+            <button type="submit">Google+</button>
+        </form>
+        <form id="guest" action="">
+            <button type="submit">Enter as Guest</button>
+        </form>
+    </div>
+    </div>
+`;
+
 function scrollToBottom(cssSelector){
     var objDiv = document.querySelector(cssSelector);
     objDiv.scrollTop = objDiv.scrollHeight;
 }
 
+function randNumber(limit){ // starts from 1 upto limit
+    return Math.floor(Math.random()*limit) + 1;
+}
+
+function formatTextForDisplay(txt){
+    return txt.trim().split("").map(function(letter){
+        if (letter==="\n")  return "<br>";
+        return letter;
+    }).join("");
+}
+
 function main(){
-    var user = 'guest';
-    var socket = io('http://localhost:3001');
+    var socket = io('http://localhost:3000');
+    var user = {};
 
     // submit message
-    document.querySelector('.type-message form').onsubmit = function(e){
-        var input = document.querySelector('.type-message input');
-        var msg = input.value;
-        if (!msg)  return false;
-        socket.emit('chat message', {
-            nickname: "shiv",
-            type: "public",
-            msg: msg
-        });
-        input.value = '';
-        return false;
+    document.querySelector('.type-message form').onkeypress = function(e){
+        if (e.which === 13 && !e.shiftKey){
+            var input = e.target;
+            var msg = formatTextForDisplay(input.value);
+            if (!msg)  return false;
+            socket.emit('chat message', {
+                nickname: "shiv",
+                type: "public",
+                msg: msg
+            });
+            input.value = "";
+            return false;
+        }  
     };
 
     // socket events
     socket.on('chat message', function(data){
-        var msgItem = msgTemplate('./imgs/avatars/dummy/145859.svg', data.nickname, data.msg, data.timestamp);
+        var msgItem = msgTemplate(`./imgs/avatars/dummy/${randNumber(10)}.svg`, data.nickname, data.msg, data.timestamp);
         document.querySelector('.chatroom .messages').innerHTML += msgItem; 
         scrollToBottom('.chatroom .messages');
     });
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
 });
